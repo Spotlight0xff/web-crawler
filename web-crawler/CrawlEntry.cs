c2798f1684c32a94b2c2f8bf5cc946fb040c713e
Spotlight0xff <spotlight0xff@gmail.com>
2015-11-11 03:21:34 +0100

    simple class to store the entries

---
+++ web-crawler/CrawlEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace webcrawler
+{
+	public class CrawlEntry
+	{
+		string entry;
+		string referrer;
+		Uri uri;
+
+		public CrawlEntry (string url)
+		{
+			referrer = "";
+			entry = url;
+		}
+
+		public CrawlEntry(Uri uri) {
+			this.uri = uri;
+			entry = uri.AbsoluteUri;
+		}
+
+		public override string ToString ()
+		{
+			if (entry != "") {
+				return entry;
+			} else {
+				return uri.ToString ();
+			}
+		}
+	}
+}
+
